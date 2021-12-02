e3eac2f006913451ae3d0496c4a475ee66f00f9a
Ele Cabrera <jai690@hotmail.com>
2021-12-02 17:58:18 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const XMLHttpRequest = require('XMLHttpRequest');
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+//usage:
+readTextFile("./Data/SistemaS2.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+});
