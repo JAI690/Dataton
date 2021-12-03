709d5303085db53a2249810a126d901650191868
Ele Cabrera <jai690@hotmail.com>
2021-12-02 22:07:43 -0600

    rutas para los datos

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,22 +4,35 @@ const router = express.Router();
 
 const { convertir,recorrer } = require('../funciones/suma');
 
+var fs=require('fs');
+
 router.get('/', (req,res) => {
-    var fs=require('fs');
-    var data = fs.readFileSync('./Data/SistemaS2.json', 'utf8');
-    var dato = JSON.parse(data);
 
-    var data2 = fs.readFileSync('./Data/Sistema3Servidores.json', 'utf8');
-    var dato2 = JSON.parse(data2);
-
-    var data3 = fs.readFileSync('./Data/declaraciones.json', 'utf8');
-    var dato3 = JSON.parse(data3);
-
-    const datos = convertir(dato,'id');
-    console.log(dato2);
+    //const datos1 = convertir(dato,'id');
 
     //console.log(datos);
-    res.render("../views/dashboard/datos.hbs", {dato});
+    res.render("../views/dashboard/datos.hbs");
+});
+
+router.get('/sanciones', (req,res) => {
+    var data2 = fs.readFileSync('./Data/Sistema3Servidores.json', 'utf8');
+    var dato2 = JSON.parse(data2);
+    console.log(dato2[0]);
+    res.render("../views/dashboard/sanciones.hbs", {dato2});
+});
+
+router.get('/declaraciones', (req,res) => {
+    var data3 = fs.readFileSync('./Data/declaraciones.json', 'utf8');
+    var dato3 = JSON.parse(data3);
+    console.log(dato3[0]);
+    res.render("../views/dashboard/declaraciones.hbs", {dato3});
+});
+
+router.get('/servidores', (req,res) => {
+    var data = fs.readFileSync('./Data/SistemaS2.json', 'utf8');
+    var dato = JSON.parse(data);
+    console.log(dato[0]);
+    res.render("../views/dashboard/servidores.hbs", {dato});
 });
 
 module.exports = router;
