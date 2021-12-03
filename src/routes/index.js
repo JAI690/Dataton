d38b8894cac4fb677767f329c6b79cc93ac8136f
Ele Cabrera <jai690@hotmail.com>
2021-12-02 18:10:35 -0600

    main layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,res) => {
+    var fs=require('fs');
+    var data = fs.readFileSync('./Data/SistemaS2.json', 'utf8');
+    var words = JSON.parse(data);
+    console.log(words); 
     res.send("Hola");
 });
 
